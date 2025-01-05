ab3254d29af7eecb9184bcb06c981f4d525a194f
dnemirich <dnemirich@gmail.com>
2025-01-05 23:07:03 +0100

    feat: solve BST task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -58,7 +58,51 @@ class BinarySearchTree {
     } else return findVal(data, this.rootNode);
   }
 
-  remove(data) { }
+  findWithParent(data) {
+    let cur = this.rootNode;
+    let parent = null;
+    while (cur) {
+      if (data === cur.data) {
+        return { node: cur, parent };
+      } else if (data > cur.data) {
+        parent = cur;
+        cur = cur.right;
+      } else {
+        parent = cur;
+        cur = cur.left;
+      }
+    }
+
+    return null;
+  }
+
+  remove(data) {
+    const res = this.findWithParent(data);
+    if (!res) return;
+
+    const { node, parent } = res;
+
+    const deleteNode = (node, parent, newNode) => {
+      if (!parent) this.rootNode = newNode;
+      else if (parent.right === node) parent.right = newNode;
+      else if (parent.left === node) parent.left = newNode;
+    };
+
+    if (!node.left && !node.right) deleteNode(node, parent, null);
+    else if (!node.left || !node.right) {
+      const newChild = node.left || node.right;
+      deleteNode(node, parent, newChild);
+    } else {
+      let newChild = node.right;
+      let newParent = node;
+      while (newChild.left) {
+        newParent = newChild;
+        newChild = newChild.left;
+      }
+      node.data = newChild.data;
+      deleteNode(newChild, newParent, newChild.right);
+    }
+  }
 
   min() {
     const findMin = (node) => {
